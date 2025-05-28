522e42b6065056d461327a716207ba654966b68e
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-05-28 01:19:43 +0100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -104,11 +104,11 @@ elif choice == "Order History":
     st.subheader("📜 Order History")
     try:
         query = """
-            SELECT o.order_id, c.name AS customer, o.order_date, o.totalamount,
-                   p.productname, od.quantity, od.subtotal
+            SELECT o.order_id, c.name AS customer, o.order_date, oi.quantity * oi.unit_price AS total_amount,
+                   p.name, oi.quantity, oi.unit_price
             FROM orders o
             JOIN customers c ON o.customer_id = c.customer_id
-            JOIN order_items od ON o.order_id = od.order_id
+            JOIN order_items od ON oi.order_item_id = oi.order_item_id
             JOIN products p ON od.product_id = p.product_id
             ORDER BY o.order_date DESC
         """
